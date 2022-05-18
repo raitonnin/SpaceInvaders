fba1136a906a4ec05f3b76e93fa59e46e06e77b9
raitonnin <45186265+raitonnin@users.noreply.github.com>
2022-05-17 23:53:37 -0500

    Animated Invaders
    Set up invaders animation for prefab variants 1 through 3
    opened up new script for invader parent script of the larger cluster of invaders as a grid object.

---
+++ SpaceInvaders/Assets/Scripts/Invader.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Invader : MonoBehaviour
+{
+    public Sprite[] animationSprites;
+
+    public float animationTime = 1.0f;
+
+    private SpriteRenderer _spriteRenderer;
+    private int _animationFrame;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), animationTime, animationTime);
+    }
+
+    private void AnimateSprite()
+    {
+        _animationFrame++;
+
+        if  (_animationFrame >= animationSprites.Length){
+             _animationFrame = 0;
+        }
+
+        _spriteRenderer.sprite = animationSprites[_animationFrame];
+    }
+}
