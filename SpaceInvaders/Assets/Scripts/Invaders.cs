9fcaf94f9884e771f508f8781b91118c02df54ba
raitonnin <45186265+raitonnin@users.noreply.github.com>
2022-06-30 02:15:02 -0500

    stuff were eddited

--- SpaceInvaders/Assets/Scripts/Invaders.cs
+++ SpaceInvaders/Assets/Scripts/Invaders.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Invaders : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Invader[] prefabs;
+    public int rows = 5;
+    public int columns = 11;
+    private void Awake()
     {
-        
-    }
+        for (int row = 0; row <this.rows; row++)
+        {
+            float width = 2.0f * (this.columns - 1);
+            float height = 2.0f * (this.rows - 1);
+            Vector3 centering = new Vector2(-width / 2, -height / 2);
+            Vector3 rowPosition = new Vector3(centering.x, centering.y +(row * 2.0f), 0.0f);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            for (int col = 0; col < this.columns; col++)
+            {
+                Invader invader = Instantiate(this.prefabs[row], this.transform);
+                Vector3 position = rowPosition;
+                position.x += col * 2.0f;
+                invader.transform.localPosition = position;
+            }
+        }
     }
 }
